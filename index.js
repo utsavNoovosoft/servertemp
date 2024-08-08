eca3ec006d7d2f20ca6e40be759b111db3a80116
utsavNoovosoft <utsav.mehta@noovosoft.com>
2024-08-08 14:34:44 +0530

    addded name

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.set("view engine","ejs")
 app.get("/",(req,res)=>{
     res.render("index",{text :"world"})
 })
-//this is added by me 
+//this is added by me
+//utsav
 const userRouter = require("./routes/users")
 app.use("/users",userRouter)
 
